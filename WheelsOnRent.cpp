e4f2df22cd87117489313ded3ae41f866fd92920
Dhruv Khandelwal <138669833+Dhruv1184@users.noreply.github.com>
2024-09-09 14:25:29 +0530

    Update WheelsOnRent.cpp

--- WheelsOnRent.cpp
+++ WheelsOnRent.cpp
@@ -4,42 +4,7 @@
 #include <cstdio>
 using namespace std;
 
-class Vehicle{
-    private:
-        string type;
-        int price;
-        string color;
 
-    public:
-        string get_type(){
-            return type;
-        }
-        
-        int get_price(){
-            return price;
-        }
-        
-        string get_color(){
-            return color;
-        }
-};
-
-class Customer{
-    private:
-        string name;
-        int age;
-        string address;
-    public:
-        string get_name(){
-            return name;
-        }
-        int get_age(){
-            return age;
-        }
-        string get_address(){
-            return address;
-        }
-};
 int main(){
     cout<<"Hello World";
 
