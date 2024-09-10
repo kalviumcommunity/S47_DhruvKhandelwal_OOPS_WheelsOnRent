6b8814012342a82a7da7341ecb3c30a2826d7e61
Dhruv1184 <dhruvkhandelwal1184@gmail.com>
2024-09-10 23:12:23 +0530

    Array of object created

--- WheelsOnRent.cpp
+++ WheelsOnRent.cpp
@@ -1,57 +1,83 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <cstdio>
 using namespace std;
 
-class Vehicle{
-    private:
-        string type;
-        int price;
-        string color;
+class Vehicle {
+private:
+    string type;
+    int price;
+    string color;
 
-    public:
-        Vehicle(string type, int price, string color){
-            this->type = type;
-            this->price = price;
-            this->color = color;
-        }
-        string get_type(){
-            return type;
-        }
-        
-        int get_price(){
-            return price;
-        }
-        
-        string get_color(){
-            return color;
-        }
+public:
+    // Default constructor to allow creating arrays of Vehicle objects
+    Vehicle() : type(""), price(0), color("") {}
+
+    // Parameterized constructor
+    Vehicle(string type, int price, string color) {
+        this->type = type;
+        this->price = price;
+        this->color = color;
+    }
+
+    string get_type() {
+        return type;
+    }
+
+    int get_price() {
+        return price;
+    }
+
+    string get_color() {
+        return color;
+    }
 };
 
-class Customer{
-    private:
-        string name;
-        int age;
-        string address;
-    public:
-        Customer(string name, int age, string address){
-            this->name = name;
-            this->age = age;
-            this->address = address;
-        }
-        string get_name(){
-            return name;
-        }
-        int get_age(){
-            return age;
-        }
-        string get_address(){
-            return address;
-        }
+class Customer {
+private:
+    string name;
+    int age;
+    string address;
+
+public:
+    Customer(string name, int age, string address) {
+        this->name = name;
+        this->age = age;
+        this->address = address;
+    }
+
+    string get_name() {
+        return name;
+    }
+
+    int get_age() {
+        return age;
+    }
+
+    string get_address() {
+        return address;
+    }
 };
-int main(){
-    cout << "Hello World";
+
+int main() {
+    string type, color;
+    int price;
+
+    // Creating an array of 3 Vehicle objects
+    Vehicle vehicles[3] = {
+        Vehicle("Car", 100000, "Red"),
+        Vehicle("Truck", 200000, "Blue"),
+        Vehicle("SUV", 150000, "Green")
+    };  
+
+    for (int i = 0; i < 3; i++) {
+        cout << "Type: " << vehicles[i].get_type() << endl;
+        cout << "Price: " << vehicles[i].get_price() << endl;
+        cout << "Color: " << vehicles[i].get_color() << endl;
+        cout << endl;
+    }
+
+    
 
     return 0;
 }
