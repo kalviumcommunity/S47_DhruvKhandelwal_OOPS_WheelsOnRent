fddea6a66a2ee6fed7c2e817b618450d8f9f4d3c
Dhruv1184 <dhruvkhandelwal1184@gmail.com>
2024-09-30 14:38:36 +0530

    Static variable

--- WheelsOnRent.cpp
+++ WheelsOnRent.cpp
@@ -8,16 +8,14 @@ private:
     string type;
     int price;
     string color;
-
+    static int vehicle_count;
 public:
-    // Default constructor to allow creating arrays of Vehicle objects
-    Vehicle() : type(""), price(0), color("") {}
 
-    // Parameterized constructor
     Vehicle(string type, int price, string color) {
         this->type = type;
         this->price = price;
         this->color = color;
+        vehicle_count++;
     }
 
     string get_type() {
@@ -31,19 +29,25 @@ public:
     string get_color() {
         return color;
     }
+
+    int get_vehicle_count() {
+        return vehicle_count;
+    }
 };
+
 
 class Customer {
 private:
     string name;
     int age;
     string address;
-
+    static int customer_count;
 public:
     Customer(string name, int age, string address) {
         this->name = name;
         this->age = age;
         this->address = address;
+        customer_count++;
     }
 
     string get_name() {
@@ -57,7 +61,15 @@ public:
     string get_address() {
         return address;
     }
+
+    int get_customer_count() {
+        return customer_count;
+    }
 };
+
+
+int Vehicle::vehicle_count = 0;
+int Customer::customer_count = 0;
 
 int main() {
     string type, color;
@@ -83,9 +95,12 @@ int main() {
     cout << "Age: " << customer->get_age() << endl;
     cout << "Address: " << customer->get_address() << endl;
 
+    cout << "Vehicle count: " << vehicles->get_vehicle_count() << endl;
+    cout << "Customer count: " << customer->get_customer_count() << endl;
     cout<<"Before delete"<<customer<<endl;
-    
+
     delete customer;
+    customer = nullptr;
 
     cout<<"After delete"<<customer;
     
